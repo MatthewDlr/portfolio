bf7b0bde59f8ef5999c4a99dac9ba7296ab9656c
Matthieu <matthieu.delarue5@gmail.com>
2024-07-08 08:34:49 +0200

    ⬆️ Upgrade nextjs to v14 & update tailwindcss

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html
       lang="en"
       className="!scroll-smooth"
+      suppressHydrationWarning={true}
     >
       <body
         className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
